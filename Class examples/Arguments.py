30b5e6e15ad718cc2df08e3d5c08d6e220d09c4f
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-08-19 22:53:26 +0530

    Update Arguments.py

--- Class examples/Arguments.py
+++ Class examples/Arguments.py
@@ -19,3 +19,33 @@
 # test1(a=10,b=20)
 # test1(b=20,a=10)
 
+# def test(*a):
+#     for i in a:
+#         print(i)
+# test(10,20,30,40,50)
+
+# def sum(*n):
+#     res = 0
+#     for i in n: res+=i
+#     print(res)
+# sum(10,20,30,40,50)
+
+
+# def sum(*n):
+#     print(type(n))
+#     res = 0
+#     for i in n: res+=i
+#     print(res)
+# sum(10,20,30,40,50)
+
+# def printer(*n):
+#     for i in n: print(i)
+# printer('sid','dev','gupta')
+
+# def test(**kargs):
+#     for i,j in kargs.items(): print(i,":",j)
+# test(name=10,date=20)
+
+def test(*a,**b):
+    print(a)
+test(10,b=20)
